f8d6034c0bad071b9aa9ba13acaed84feaf04397
code-tieumomo <code.tieumomo@gmail.com>
2024-03-05 20:46:40 +0700

    prepare

--- firebase.js
+++ firebase.js
@@ -5,10 +5,11 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 const firebaseConfig = {
     apiKey: "AIzaSyAydqGt2q6IpEQFSkqCflJAYjZEUJNt5gQ",
     authDomain: "jsi03-46706.firebaseapp.com",
+    databaseURL: "https://jsi03-46706-default-rtdb.firebaseio.com",
     projectId: "jsi03-46706",
     storageBucket: "jsi03-46706.appspot.com",
     messagingSenderId: "967629567676",
-    appId: "1:967629567676:web:dc6b3d069fed42ec8182b9",
+    appId: "1:967629567676:web:dc6b3d069fed42ec8182b9"
 };
 
 // Khởi tạo ứng dụng với config đã cung cấp bên trên
